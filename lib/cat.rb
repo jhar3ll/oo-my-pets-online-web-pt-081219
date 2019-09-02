71dde9c508de7ba988094dfb97245e49bb18d9b6
Jharell Chambers <jharell.chambers@selu.edu>
2019-09-02 19:13:05 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,9 +2,10 @@ class Cat
   attr_reader :name 
   attr_accessor :owner 
   
-  def initialize(name, owner)
+  def initialize(name, owner, mood="nervous")
     @name = name 
     @owner = owner
+    @mood = mood
   end 
   
 end
